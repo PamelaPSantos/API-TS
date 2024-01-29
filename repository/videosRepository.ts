dfb46a22ec528e5d6b5e55bd65bedd7dcd1783eb
Pamela Pereira dos Santos <pamela.santos@samisaude.com>
2024-01-29 18:09:01 -0300

    feat/add-repos

---
+++ repository/videosRepository.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { Video } from "../models/videos";
+
+const VideoSchema = new mongoose.Schema<Video>({
+  titulo: {String},
+  texto: {String},
+  imagem: {String},
+  duracao: {String},
+  link: {String},
+  url: {String},
+  dataPublicacao: {Date},
+  ativo: {Boolean}
+})
+
+export const videosRepository = mongoose.model<Video>("videos", VideoSchema)
