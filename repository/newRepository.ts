8732aad27951393028afcfb811e4aba42a4fb73b
Pamela Pereira dos Santos <pamela.santos@samisaude.com>
2024-01-29 18:23:45 -0300

    add models and repos

--- repository/newRepository.ts
+++ repository/newRepository.ts
@@ -1,4 +1,4 @@
 import mongoose from "mongoose"
-import { NewSchema } from "../models/newSchema"
+import { NewSchema } from "../models/new"
 
 export const newRepository = mongoose.model("new", NewSchema )
