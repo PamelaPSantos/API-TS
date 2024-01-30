0239f5a405660d349c2c3a8029d405e7d422ca0b
Pamela Pereira dos Santos <pamela.santos@samisaude.com>
2024-01-30 17:38:47 -0300

    compilados

---
+++ dist/models/core.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Core = void 0;
+class Core {
+}
+exports.Core = Core;
--- dist/models/new.js
+++ dist/models/new.js
@@ -3,15 +3,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.newRepository = void 0;
+exports.NewSchema = exports.New = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-const newSchema = new mongoose_1.default.Schema({
+const core_1 = require("../models/core");
+class New extends core_1.Core {
+}
+exports.New = New;
+exports.NewSchema = new mongoose_1.default.Schema({
     titulo: { type: String },
     texto: { type: String },
     autor: { type: String },
     imagem: { type: String },
     dataPublicacao: { type: String },
     link: { type: String },
-    ativo: { type: Boolean }
+    ativo: { Boolean }
 });
-exports.newRepository = mongoose_1.default.model("new", newSchema);
